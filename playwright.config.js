45dedffeed70b555648ecbc7f4fe33bf64edd468
shubhamkgupta14 <skshubham0786@gmail.com>
2022-03-12 12:49:00 +0530

    adding allure-report

--- playwright.config.js
+++ playwright.config.js
@@ -1,6 +1,17 @@
 // @ts-check
 const { devices } = require("@playwright/test");
 
+/**
+ * Read environment variables from file.
+ * https://github.com/motdotla/dotenv
+ */
+// require('dotenv').config();
+
+/**
+ * @see https://playwright.dev/docs/test-configuration
+ * @type {import('@playwright/test').PlaywrightTestConfig}
+ */
+
 const config = {
   testDir: "./tests",
   timeout: 30 * 1000,
@@ -25,6 +36,7 @@ const config = {
   use: {
     actionTimeout: 0,
     trace: "on-first-retry",
+    screenshot: "on"
   },
 
   projects: [
